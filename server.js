18755accb7c9ff19ac2d531bb10ac8afc9775440
Snixells <bschr9uwb@googlemail.com>
2018-03-25 14:31:12 +0200

    Implementing => Basic Server, Handling Post, Sending Back "It Works" in JSON
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./app.js');
+
+// Use Environment variable if set, else 3000
+const port = process.env.port || 3000;
+
+// Settin up express Server, listening on defined port
+app.listen(port, () => {
+    console.log('Listening on Port ' + port);   
+});
