cd943fdf0ff94d0fe2119ac1e5e5339258a41566
Snixells <bschr9uwb@googlemail.com>
2018-03-25 23:39:11 +0200

    Adding => Headers
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.use(morgan('dev'));
 app.use((bodyParser.urlencoded({extended: true})));
 app.use(bodyParser.json());
 
+// Adding Headers to response -> to not get CORS Errors
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    // OPTIONS request is sent by browsers to see which options are available => in this case PUT, POST, PATCH, DELETE, GET
+    if(request === 'OPTIONS'){
+        response.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return response.status(200).json({});
+    }
+
+    // Next is needed so that the following code (routes, e.g.) will be executed
+    next();
+});
+
 // All the requests to /producs will be forwarded to productRoutes -> see const above
 app.use('/products', productRoutes);
 
