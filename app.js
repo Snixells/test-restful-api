b61003270aebd2598e52ad902ac16cf0a3501147
Snixells <bschr9uwb@googlemail.com>
2018-03-25 15:34:04 +0200

    Implementing => Throwing Errors when not found or when errors occur in general
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

--- app.js
+++ app.js
@@ -13,4 +13,19 @@ app.use('/products', productRoutes);
 
 // All the requests to /orders will be forwarded to orderRoutes -> see const above
 app.use('/orders', orderRoutes);
+
+app.use((request, response, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, request, response, next) => {
+    response.status(error.status || 500);
+    response.json({
+        error: {
+            message:error.message
+        }
+    })
+})
 module.exports = app;
