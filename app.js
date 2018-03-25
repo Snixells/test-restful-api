b6a431cbd06f6ebbbea2a1f88d9ef50de6c26be8
Snixells <bschr9uwb@googlemail.com>
2018-03-25 15:24:35 +0200

    Npm install => morgan, tool for logging nodejs servers
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+// Morgen = Node Server Logging Tool
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // All the requests to /producs will be forwarded to productRoutes -> see const above
 app.use('/products', productRoutes);
